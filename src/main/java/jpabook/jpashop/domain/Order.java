b90b1ac6fab6666e5f515c3c3427d76954c2f55f
gussla2206 <gussla2206@gmail.com>
2023-11-12 18:21:11 +0900

    session2) Entity Class 설계 시 주의점

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,15 +17,15 @@ public class Order {
     @Column(name = "order_id")  // 컬럼이름을 테이블 명에 id로 해둠
     private Long id;
 
-    @ManyToOne  // Order랑 Member는 다대일 관계
+    @ManyToOne(fetch = FetchType.LAZY)  // Order랑 Member는 다대일 관계
     @JoinColumn(name = "member_id")  // member_id 로 조인함. FK 아이디가 되는 부분
     // 연관관계 주인 설정 필요 ) 값이 변경이 되었을 때 FK를 변경해주는 것이 연관관계의 주인
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id") // 일대일 관계에서 연관관계 주인은 Order!
     private Delivery delivery;
 
@@ -34,5 +34,18 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    // ==연관관계 메서드 == //
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
