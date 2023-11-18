edaa117d483cde0077744add532f754af44014cf
gussla2206 <gussla2206@gmail.com>
2023-11-18 17:09:03 +0900

    회원등록

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "/members/createMemberForm";
+
+    }
+    @PostMapping("/members/new") // BindingResult 는 원래 오류가 있으면 튕겨져 나가는데, 이걸 선언할 시에 오류가 담겨서 해당 메서드가 실행이 되게함
+    public String create(@Valid MemberForm form, BindingResult result){ //@Valid 는 validation 으로 정의한 걸 사용 가능하게 만듦
+
+        if(result.hasErrors()){
+            return "/members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
